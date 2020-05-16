1ea8043a8c01355e995714e6a295b78261ff9ce4
Mark Brito <mark.brito.gcloud@gmail.com>
2020-05-15 20:20:49 -0600

    iptables update

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -10,7 +10,10 @@
  #  iptables -A INPUT -p tcp --dport 5353 -j ACCEPT
  #  iptables -A INPUT -p udp --dport 53 -j ACCEPT
    #  iptables -A INPUT -p udp --dport 5353 -j ACCEPT
-   iptables -A INPUT -s 40.121.213.9 -p tcp --dport 9996 -j ACCEPT
+   iptables -A INPUT -p tcp --sport 9996 -j ACCEPT
+   iptables -A INPUT -p tcp --sport 9995 -j ACCEPT
+   iptables -A INPUT -p tcp --dport 9996 -j ACCEPT
+   iptables -A INPUT -p tcp --dport 9995 -j ACCEPT
    iptables -A INPUT -s 192.168.43.1 -p tcp --sport 8022 -j ACCEPT
    iptables -A INPUT -p tcp --sport 2222 -j ACCEPT
    iptables -A INPUT -p tcp -s 127.0.0.1 --dport 631 -j ACCEPT
@@ -62,7 +65,10 @@ iptables -A INPUT -s 127.0.0.1 -j ACCEPT
 #   iptables -A OUTPUT -p tcp --dport 5353 -j ACCEPT
 #   iptables -A OUTPUT -p udp --dport 5353 -j ACCEPT
 #   iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-   iptables -A OUTPUT -d 40.121.213.9 -p tcp --sport 9996 -j ACCEPT
+   iptables -A OUTPUT -p tcp --dport 9996 -j ACCEPT
+   iptables -A OUTPUT -p tcp --dport 9995 -j ACCEPT
+   iptables -A OUTPUT -p tcp --sport 9996 -j ACCEPT
+   iptables -A OUTPUT -p tcp --sport 9995 -j ACCEPT
    iptables -A OUTPUT -d 192.168.43.1 -p tcp --dport 8022 -j ACCEPT
    iptables -A OUTPUT -p tcp --dport 2222 -j ACCEPT
    iptables -A OUTPUT -p tcp -d 127.0.0.1 --sport 631 -j ACCEPT
